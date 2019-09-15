3c003f741b03832e6250318a38e8595f79826b2a
lukas_m@gmx.de <lukas_m@gmx.de>
2019-09-15 19:59:47 +0200

    Major changes. - Removed unnecessary code for other microcontrollers. - Added timer interrupt (teensy) which executes the loop function to read and write. - Modifications in order to read one byte instead of full message per interrupt - Writing one byte per interrupt is missing. - Renamed some variables.
    Changes are completly untested.

--- src/iBus.cpp
+++ src/iBus.cpp
@@ -1,246 +1,201 @@
 /*
- * Interface to the RC iBus protocol
- * 
- * Based on original work from: https://gitlab.com/timwilkinson/FlySkyIBus
- * Extended to also handle sensors/telemetry data to be sent back to the transmitter,
- * interrupts driven and other features.
- *
- * This lib requires a hardware UART for communication
- * Another version using software serial is here https://github.com/Hrastovc/iBUStelemetry
- * 
- * Explaination of sensor/ telemetry prtocol here: 
- * https://github.com/betaflight/betaflight/wiki/Single-wire-FlySky-(IBus)-telemetry
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public 
- * License as published by the Free Software Foundation; either  
- * version 2.1 of the License, or (at your option) any later version.
- *   
- * Created 12 March 2019 Bart Mellink
- * Updated 4 April 2019 to support ESP32
- */
+* Interface to the RC iBus protocol
+*
+* Based on original work from: https://gitlab.com/timwilkinson/FlySkyIBus
+* Extended to also handle sensors/telemetry data to be sent back to the transmitter,
+* interrupts driven and other features.
+*
+* Explanation of sensor/ telemetry protocol here:
+* https://github.com/betaflight/betaflight/wiki/Single-wire-FlySky-(IBus)-telemetry
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* Created 12 March 2019 Bart Mellink
+* Updated 4 April 2019 to support ESP32
+* Modified 14 September 2019 by Lukas
+*/
 
-#include <Arduino.h>
 #include "iBus.h"
 
-// pointer to the first class instance to be used to call the loop() method from timer interrupt
-// will be initiated by class constructor, then daisy channed to other class instances if we have more than one
-IBUS* iBus_first = NULL;
+#include <Arduino.h>
 
-
-// Interrupt on timer0 - called every 1 ms
-// we call the iBusSensor.loop() here, so we are certain we respond to sensor requests in a timely matter
-#ifdef ARDUINO_ARCH_AVR
-SIGNAL(TIMER0_COMPA_vect) {
-  if (iBus_first) iBus_first->loop();  // gets new servo values if available and process any sensor data
+// timer interrupt
+void  timer_isr() {
+	loop();
 }
-#elif defined _VARIANT_ARDUINO_STM32_
-void  onTimer(stimer_t *htim) {
-  if (iBus_first) iBus_first->loop();  // gets new servo values if available and process any sensor data
-}
-#else
-void  onTimer() {
-  if (iBus_first) iBus_first->loop();  // gets new servo values if available and process any sensor data
-}
-#endif
 
 /*
- *  supports max 14 channels in this lib (with messagelength of 0x20 there is room for 14 channels)
+lib supports a max of 14 channels (with a protocol length of 0x20 there is room for 14 channels)
 
-  Example set of bytes coming over the iBus line for setting servos: 
-    20 40 DB 5 DC 5 54 5 DC 5 E8 3 D0 7 D2 5 E8 3 DC 5 DC 5 DC 5 DC 5 DC 5 DC 5 DA F3
-  Explanation
-    Protocol length: 20
-    Command code: 40 
-    Channel 0: DB 5  -> value 0x5DB
-    Channel 1: DC 5  -> value 0x5Dc
-    Channel 2: 54 5  -> value 0x554
-    Channel 3: DC 5  -> value 0x5DC
-    Channel 4: E8 3  -> value 0x3E8
-    Channel 5: D0 7  -> value 0x7D0
-    Channel 6: D2 5  -> value 0x5D2
-    Channel 7: E8 3  -> value 0x3E8
-    Channel 8: DC 5  -> value 0x5DC
-    Channel 9: DC 5  -> value 0x5DC
-    Channel 10: DC 5 -> value 0x5DC
-    Channel 11: DC 5 -> value 0x5DC
-    Channel 12: DC 5 -> value 0x5DC
-    Channel 13: DC 5 -> value 0x5DC
-    Checksum: DA F3 -> calculated by adding up all previous bytes, total must be FFFF
- */
+Example set of bytes coming over the iBus line:
+20 40 DB 5 DC 5 54 5 DC 5 E8 3 D0 7 D2 5 E8 3 DC 5 DC 5 DC 5 DC 5 DC 5 DC 5 DA F3
+Explanation
+Protocol length: 0x20
+Command code: 40
+Channel 0: DB 5  -> value 0x5DB
+Channel 1: DC 5  -> value 0x5Dc
+Channel 2: 54 5  -> value 0x554
+Channel 3: DC 5  -> value 0x5DC
+Channel 4: E8 3  -> value 0x3E8
+Channel 5: D0 7  -> value 0x7D0
+Channel 6: D2 5  -> value 0x5D2
+Channel 7: E8 3  -> value 0x3E8
+Channel 8: DC 5  -> value 0x5DC
+Channel 9: DC 5  -> value 0x5DC
+Channel 10: DC 5 -> value 0x5DC
+Channel 11: DC 5 -> value 0x5DC
+Channel 12: DC 5 -> value 0x5DC
+Channel 13: DC 5 -> value 0x5DC
+Checksum: DA F3 -> calculated by adding up all previous bytes, total must be FFFF
+*/
 
 
-void IBUS::begin(HardwareSerial& serial, int8_t timerid, int8_t rxPin, int8_t txPin) {
-  #ifdef ARDUINO_ARCH_ESP32
-    serial.begin(115200, SERIAL_8N1, rxPin, txPin);
-  #else
-    serial.begin(115200, SERIAL_8N1);
-  #endif
+void IBUS::begin(HardwareSerial& serial, IntervalTimer timer, int8_t rxPin, int8_t txPin) {
 
-  this->stream = &serial;
-  this->state = DISCARD;
-  this->last = millis();
-  this->ptr = 0;
-  this->len = 0;
-  this->chksum = 0;
-  this->lchksum = 0;
+	serial.begin(115200, SERIAL_8N1);
 
-  // we need to process the iBus sensor protocol handler frequently enough (at least once each ms) to ensure the response data
-  // from the sensor is sent on time to the receiver
-  // if timerid==IBUS_NOTIMER the user is responsible for calling the loop function
-  this->iBus_next = iBus_first;
+	stream = serial;
+	state = DISCARD;
+	last = millis();
+	bufferIndex = 0;
+	len = 0;
+	checksum = 0;
+	chkl = 0;
 
-  if (!iBus_first && timerid != IBUS_NOTIMER) {
-    #ifdef ARDUINO_ARCH_AVR
-      // on AVR architectures Timer0 is already used for millis() - we'll just interrupt somewhere in the middle and call the TIMER0_COMPA_vect interrupt
-      OCR0A = 0xAF;
-      TIMSK0 |= _BV(OCIE0A);
-    #else
-      // on other architectures we need to use a time
-      #if defined(ARDUINO_ARCH_ESP32) 
-        hw_timer_t * timer = NULL;
-        timer = timerBegin(timerid, F_CPU / 1000000L, true); // defaults to timer_id = 0; divider=80 (1 ms); countUp = true;
-        timerAttachInterrupt(timer, &onTimer, true); // edge = true
-        timerAlarmWrite(timer, 1000, true);  //1 ms
-        timerAlarmEnable(timer);
-      #elif defined(_VARIANT_ARDUINO_STM32_)
-	      TIM_TypeDef * TIMER = TIM1; // Select timer, TODO convert (int8_t timerid) into: (TIM_TypeDef * TIMER = TIMx)
-        static stimer_t TimHandle; // Handler for stimer
-	      TimHandle.timer = TIMER; // Set TIMx instance.
-	      TimerHandleInit(&TimHandle, 1000 - 1, ((uint32_t)(getTimerClkFreq(TIMER) / (1000000)) - 1)); // Set TIMx timer to 1ms 
-	      attachIntHandle(&TimHandle, onTimer); // Attach onTimer interupt routine 
-      #else
-        // It should not be too difficult to support additional architectures as most have timer functions, but I only tested AVR and ESP32
-        #error "Only support for AVR, ESP32 and STM32 architectures."
-      #endif
-    #endif
-  }
-  iBus_first = this; 
+	// start timer
+	timer.begin(timer_isr, TIMER_INTERVAL);
 }
 
-// called from timer interrupt or mannually by user (if IBUS_NOTIMER set in begin())
+// called from timer_isr
 void IBUS::loop(void) {
+	// only process data already in our UART receive buffer
+	if (stream.available() > 0) {
+		// only consider a new data package, if we have not heard anything for over 3ms
+		uint32_t now = millis();
+		if (now - last >= PROTOCOL_TIMEGAP){
+			state = GET_LENGTH;
+		}
+		last = now;
+		
+		rc = stream.read();
+		
+		switch (state) {
+			case GET_LENGTH:
+				if ((rc <= PROTOCOL_LENGTH) && (rc > PROTOCOL_OVERHEAD)) {
+					bufferIndex = 0;
+					len = rc - PROTOCOL_OVERHEAD;
+					checksum = 0xFFFF - rc;
+					state = GET_DATA;
+				}
+				else {
+					state = DISCARD;
+				}
+				break;
+				
+			case GET_DATA:
+				buffer[bufferIndex++] = rc;
+				checksum -= rc;
+				if (bufferIndex == len) {
+					state = GET_CHECKSUM_L;
+				}
+				break;
+			
+			case GET_CHECKSUM_L:
+				chkl = rc;
+				state = GET_CHECKSUM_H;
+				break;
 
-  // if we have multiple instances of iBus, we (recursively) call the other instances loop() function
-  if (iBus_next) iBus_next->loop(); 
-
-  // only process data already in our UART receive buffer 
-  while (stream->available() > 0) {
-    // only consider a new data package if we have not heard anything for >3ms
-    uint32_t now = millis();
-    if (now - last >= PROTOCOL_TIMEGAP){
-      state = GET_LENGTH;
-    }
-    last = now;
-    
-    uint8_t v = stream->read();
-    switch (state) {
-      case GET_LENGTH:
-        if (v <= PROTOCOL_LENGTH && v > PROTOCOL_OVERHEAD) {
-          ptr = 0;
-          len = v - PROTOCOL_OVERHEAD;
-          chksum = 0xFFFF - v;
-          state = GET_DATA;
-        } else {
-          state = DISCARD;
-        }
-        break;
-
-      case GET_DATA:
-        buffer[ptr++] = v;
-        chksum -= v;
-        if (ptr == len) {
-          state = GET_CHKSUML;
-        }
-        break;
-        
-      case GET_CHKSUML:
-        lchksum = v;
-        state = GET_CHKSUMH;
-        break;
-
-      case GET_CHKSUMH:
-        // Validate checksum
-        if (chksum == (v << 8) + lchksum) {
-          // Checksum is all fine Execute command - 
-          uint8_t adr = buffer[0] & 0x0f;
-          if (buffer[0]==PROTOCOL_COMMAND40) {
-            // Valid servo command received - extract channel data
-            for (uint8_t i = 1; i < PROTOCOL_CHANNELS * 2 + 1; i += 2) {
-              channel[i / 2] = buffer[i] | (buffer[i + 1] << 8);
-            }
-            cnt_rec++;
-          } else if (adr<=NumberSensors && adr>0 && len==1) {
-            // all sensor data commands go here
-            // we only process the len==1 commands (=message length is 4 bytes incl overhead) to prevent the case the
-            // return messages from the UART TX port loop back to the RX port and are processed again. This is extra
-            // precaution as it will also be prevented by the PROTOCOL_TIMEGAP required
-            delayMicroseconds(100);
-            switch (buffer[0] & 0x0f0) {
-              case PROTOCOL_COMMAND_DISCOVER: // 0x80, discover sensor
-                cnt_poll++;  
-                // echo discover command: 0x04, 0x81, 0x7A, 0xFF 
-                stream->write(0x04);
-                stream->write(PROTOCOL_COMMAND_DISCOVER + adr);
-                chksum = 0xFFFF - (0x04 + PROTOCOL_COMMAND_DISCOVER + adr);
-                break;
-              case PROTOCOL_COMMAND_TYPE: // 0x90, send sensor type
-                // echo sensortype command: 0x06 0x91 0x00 0x02 0x66 0xFF 
-                stream->write(0x06);
-                stream->write(PROTOCOL_COMMAND_TYPE + adr);
-                stream->write(sensorType[adr]);
-                stream->write(0x02); // always this value - unkwown
-                chksum = 0xFFFF - (0x06 + PROTOCOL_COMMAND_TYPE + adr + sensorType[adr] + 2);
-                break;
-              case PROTOCOL_COMMAND_VALUE: // 0xA0, send sensor data
-                cnt_sensor++;
-                // echo sensor value command: 0x06 0x91 0x00 0x02 0x66 0xFF 
-                stream->write(0x06);
-                stream->write(PROTOCOL_COMMAND_VALUE + adr);
-                stream->write(sensorValue[adr] & 0x0ff);
-                stream->write(sensorValue[adr] >> 8); 
-                chksum = 0xFFFF - (0x06 + PROTOCOL_COMMAND_VALUE + adr + (sensorValue[adr]>>8) + (sensorValue[adr]&0x0ff));
-                break;
-              default:
-                adr=0; // unknown command, prevent sending chksum
-                break;
-            }
-            if (adr>0) {
-              stream->write(chksum & 0x0ff);
-              stream->write(chksum >> 8);              
-            }
-          }
-        }
-        state = DISCARD;
-        break;
-
-      case DISCARD:
-      default:
-        break;
-    }
-  }
+			case GET_CHECKSUM_H:
+				// validate checksum
+				if (checksum == (rc << 8) + chkl) {
+					// extract sensor address
+					adr = buffer[0] & 0x0F;
+					// validate command
+					if (buffer[0] == PROTOCOL_COMMAND40) {
+						// extract channel data
+						for (uint8_t i = 1; i < PROTOCOL_CHANNELS * 2 + 1; i += 2) {
+							channel[i / 2] = buffer[i] | (buffer[i + 1] << 8);
+						}
+						cnt_channelMessage++;
+					} 
+					else if ((adr <= sensorNumber) && (adr > 0) && (len == 1)) {
+						/*
+						All sensor data commands. 
+						Only process len == 1 (packet length is 4 bytes (with overhead)) to prevent the case the
+						return messages from the UART TX port loop back to the RX port and are processed again. This is extra
+						precaution, since it will also be prevented by PROTOCOL_TIMEGAP
+						*/
+						//delayMicroseconds(100);
+						
+						// TODO: Change to write only one byte per loop / timer interrupt
+						switch (buffer[0] & 0xF0) {
+							case PROTOCOL_COMMAND_DISCOVER: // 0x80, discover sensor
+								// echo discover command: 0x04, 0x81, 0x7A, 0xFF
+								stream.write(0x04);
+								stream.write(PROTOCOL_COMMAND_DISCOVER + adr);
+								checksum = 0xFFFF - (0x04 + PROTOCOL_COMMAND_DISCOVER + adr);
+								cnt_pollMessage++;
+								break;
+							case PROTOCOL_COMMAND_TYPE: // 0x90, send sensor type
+								// echo sensor type command: 0x06 0x91 0x00 0x02 0x66 0xFF
+								stream.write(0x06);
+								stream.write(PROTOCOL_COMMAND_TYPE + adr);
+								stream.write(sensorType[adr]);
+								stream.write(0x02); // always this value - unknown
+								checksum = 0xFFFF - (0x06 + PROTOCOL_COMMAND_TYPE + adr + sensorType[adr] + 2);
+								break;
+							case PROTOCOL_COMMAND_VALUE: // 0xA0, send sensor data
+								// echo sensor value command: 0x06 0x91 0x00 0x02 0x66 0xFF
+								stream.write(0x06);
+								stream.write(PROTOCOL_COMMAND_VALUE + adr);
+								stream.write(sensorValue[adr] & 0xFF);
+								stream.write(sensorValue[adr] >> 8);
+								checksum = 0xFFFF - (0x06 + PROTOCOL_COMMAND_VALUE + adr + (sensorValue[adr] >> 8) + (sensorValue[adr] & 0xFF));
+								cnt_sentMessage++;
+								break;
+							default:
+								adr = 0; // unknown command, prevent sending checksum
+								break;
+						}
+						if (adr > 0) {
+							stream.write(checksum & 0xFF);
+							stream.write(checksum >> 8);
+						}
+					}
+				}
+				state = DISCARD;
+				break;
+			case DISCARD:
+				break;
+			default:
+				break;
+		}
+	}
 }
 
-uint16_t IBUS::readChannel(uint8_t channelNr) {
-  if (channelNr < PROTOCOL_CHANNELS) {
-    return channel[channelNr];
-  } else {
-    return 0;
-  }
+uint16_t IBUS::readChannel(uint8_t channelNumber) {
+	if (channelNumber < PROTOCOL_CHANNELS) {
+		return channel[channelNumber];
+	}
+	else {
+		return 0;
+	}
 }
 
 uint8_t IBUS::addSensor(uint8_t type) {
-  // add a sensor, return sensor number
-  if (NumberSensors < SENSORMAX) {
-    NumberSensors++;
-    sensorType[NumberSensors] = type;
-  }
-  return NumberSensors;
+	// add a sensor, return sensor number
+	if (sensorNumber < SENSORMAX) {
+		++sensorNumber;
+		sensorType[sensorNumber] = type;
+	}
+	return sensorNumber;
 }
 
 void IBUS::setSensorMeasurement(uint8_t adr, uint16_t value){
-   if (adr<=NumberSensors)
-     sensorValue[adr] = value;
+	if (adr <= sensorNumber) {
+		sensorValue[adr] = value;	
+	}
 }
-
-
--- src/iBus.h
+++ src/iBus.h
@@ -1,68 +1,72 @@
 /*
- * Interface to the RC iBus protocol
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public 
- * License as published by the Free Software Foundation; either  
- * version 2.1 of the License, or (at your option) any later version.
- *   
- * Created 12 March 2019 Bart Mellink
- */
+* Interface to the RC iBus protocol
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* Created 12 March 2019 Bart Mellink
+* Modified 14 September 2019 by Lukas
+*/
+
 #ifndef IBUS_H
 #define IBUS_H
 
-#include <inttypes.h>
+//#include <inttypes.h>
 
-#define IBUSS_INTV 0 // Internal voltage (in 0.01)
-#define IBUSS_TEMP 1 // Temperature (in 0.1 degrees, where 0=-40'C)
-#define IBUSS_RPM  2 // RPM
-#define IBUSS_EXTV 3 // External voltage (in 0.01)
+#define IBUSS_INTV 0	// Internal voltage (in 0.01)
+#define IBUSS_TEMP 1	// Temperature (in 0.1 degrees, where 0=-40'C)
+#define IBUSS_RPM  2	// RPM
+#define IBUSS_EXTV 3	// External voltage (in 0.01)
 
-#define IBUS_NOTIMER -1 // no timer interrupt used
-
-class HardwareSerial;
-class Stream;
+//class HardwareSerial;
+//class Stream;
 
 class IBUS {
 
-public:
-  void begin(HardwareSerial& serial, int8_t timerid=0, int8_t rxPin=-1, int8_t txPin=-1);
-  uint16_t readChannel(uint8_t channelNr); // read servo channel 0..9
-  uint8_t addSensor(uint8_t type); // usually 2, returns address
-  void setSensorMeasurement(uint8_t adr, uint16_t value);
+	public:
+	void begin(HardwareSerial& serial, IntervalTimer timer, int8_t rxPin, int8_t txPin);
+	uint16_t readChannel(uint8_t channelNumber);	// read channel 0..9
+	uint8_t addSensor(uint8_t type);	// usually 2, returns address
+	void setSensorMeasurement(uint8_t adr, uint16_t value);
 
-  void loop(void); // used internally for interrupt handline, but needs to be defined as public
-  
-  uint8_t cnt_poll; // count received number of sensor poll messages
-  uint8_t cnt_sensor; // count times a sensor value has been sent back
-  uint8_t cnt_rec; // count received number of servo messages
-  
-private:
-  enum State {GET_LENGTH, GET_DATA, GET_CHKSUML, GET_CHKSUMH, DISCARD};
-
-  static const uint8_t PROTOCOL_LENGTH = 0x20;
-  static const uint8_t PROTOCOL_OVERHEAD = 3; // packet is <len><cmd><data....><chkl><chkh>, overhead=cmd+chk bytes
-  static const uint8_t PROTOCOL_TIMEGAP = 3; // Packets are received very ~7ms so use ~half that for the gap
-  static const uint8_t PROTOCOL_CHANNELS = 14;
-  static const uint8_t PROTOCOL_COMMAND40 = 0x40;        // Command to set servo or motor speed is always 0x40
-  static const uint8_t PROTOCOL_COMMAND_DISCOVER = 0x80; // Command discover sensor (lowest 4 bits are sensor)
-  static const uint8_t PROTOCOL_COMMAND_TYPE = 0x90;     // Command discover sensor (lowest 4 bits are sensor)
-  static const uint8_t PROTOCOL_COMMAND_VALUE = 0xA0;    // Command send sensor data (lowest 4 bits are sensor)
-  static const uint8_t SENSORMAX = 10; // Max number of sensors
-  
-  uint8_t state;  // state machine state for iBus protocol
-  HardwareSerial* stream; // serial port
-  uint32_t last;  // milis() of prior message
-  uint8_t buffer[PROTOCOL_LENGTH]; // message buffer
-  uint8_t ptr; // pointer in buffer
-  uint8_t len; // message length
-  uint16_t channel[PROTOCOL_CHANNELS]; // servo data received
-  uint16_t chksum; // checksum calculation
-  uint8_t lchksum; // checksum lower byte received
-  uint8_t sensorType[SENSORMAX];  // sensor types for defined sensors
-  uint16_t sensorValue[SENSORMAX];  // sensor data for defined sensors
-  uint8_t NumberSensors; // number of sensors
-  IBUS* iBus_next = NULL;  // pointer to the next class instance to be used to call the loop() method from timer interrupt
+	void loop(void);	// used internally for interrupt handling, but needs to be defined as public
+	
+	uint16_t cnt_pollMessage;		// count of sensor poll messages
+	uint16_t cnt_sentMessage;		// count of sent messages
+	uint16_t cnt_channelMessage;	// count of received messages
+	
+	private:
+	enum State {GET_LENGTH, GET_DATA, GET_CHECKSUM_L, GET_CHECKSUM_H, DISCARD};
+	
+	// packet is <length><cmd><data><chkl><chkh>
+	// length: number of bytes inside packet
+	static const uint8_t PROTOCOL_LENGTH = 0x20;
+	static const uint8_t PROTOCOL_OVERHEAD = 3;	// overhead: length, chkl, chkh
+	static const uint8_t PROTOCOL_TIMEGAP = 3;	// packet is received every ~7 ms, so using 3 ms for the time gap is feasible
+	static const uint8_t PROTOCOL_CHANNELS = 14;
+	static const uint8_t PROTOCOL_COMMAND40 = 0x40;	// command to send channels (always 0x40)
+	static const uint8_t PROTOCOL_COMMAND_DISCOVER = 0x80;	// command to discover sensor (4 highest bits)
+	static const uint8_t PROTOCOL_COMMAND_TYPE = 0x90;	// command for sensor type (4 highest bits)
+	static const uint8_t PROTOCOL_COMMAND_VALUE = 0xA0;	// command for sensor data (4 highest bits)
+	static const uint8_t SENSORMAX = 10;	// maximum number of sensors
+	static const uint16_t TIMER_INTERVAL = 100;	// timer interval in microseconds - There is (7 - PROTOCOL_TIMEGAP) ms for PROTOCOL_LENGTH bytes (example:  4 ms for 32 byte = 125 us for 1 byte)
+	
+	uint8_t rc;	// received byte from remote control
+	uint8_t state;	// state machine state for iBus protocol
+	uint8_t adr;	// sensor address
+	HardwareSerial stream;	// serial port
+	uint32_t last;	// ms since prior message
+	uint8_t buffer[PROTOCOL_LENGTH];	// message buffer
+	uint8_t bufferIndex;	// buffer index
+	uint8_t len;	// number of bytes for cmd, data
+	uint16_t channel[PROTOCOL_CHANNELS];	// servo data received
+	uint16_t checksum;	// calculated checksum
+	uint8_t chkl;	// lower byte of received checksum
+	uint8_t sensorType[SENSORMAX];	// sensor types for defined sensors
+	uint16_t sensorValue[SENSORMAX];	// sensor data for defined sensors
+	uint8_t sensorNumber;	// number of sensors
 };
 
 #endif
