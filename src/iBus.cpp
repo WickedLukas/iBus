164a8f5051437c509d4984927cb4dc2084f34683
lukas_m@gmx.de <lukas_m@gmx.de>
2019-09-21 09:20:59 +0200

    Major changes
    - Remove timer interrupt. iBus update is now called manually.
    - Replace what was a delay between reading and writing with writing during next update call. Therefor another state was added.
    - Renamed several variables.
    - Cleanup.
    - Code compiles now.

--- src/iBus.cpp
+++ src/iBus.cpp
@@ -20,13 +20,6 @@
 
 #include "iBus.h"
 
-#include <Arduino.h>
-
-// timer interrupt
-void  timer_isr() {
-	loop();
-}
-
 /*
 lib supports a max of 14 channels (with a protocol length of 0x20 there is room for 14 channels)
 
@@ -53,41 +46,38 @@ Checksum: DA F3 -> calculated by adding up all previous bytes, total must be FFF
 */
 
 
-void IBUS::begin(HardwareSerial& serial, IntervalTimer timer, int8_t rxPin, int8_t txPin) {
-
-	serial.begin(115200, SERIAL_8N1);
-
+void IBUS::begin(HardwareSerial& serial, int8_t rxPin, int8_t txPin) {
 	stream = serial;
+	
+	stream.begin(115200, SERIAL_8N1);
+	
 	state = DISCARD;
 	last = millis();
-	bufferIndex = 0;
-	len = 0;
-	checksum = 0;
-	chkl = 0;
-
-	// start timer
-	timer.begin(timer_isr, TIMER_INTERVAL);
+	dataIndex = 0;
+	dataLength = 0;
+	checksumCalculated = 0;
+	checksumLow = 0;
 }
 
-// called from timer_isr
-void IBUS::loop(void) {
-	// only process data already in our UART receive buffer
-	if (stream.available() > 0) {
-		// only consider a new data package, if we have not heard anything for over 3ms
-		uint32_t now = millis();
-		if (now - last >= PROTOCOL_TIMEGAP){
-			state = GET_LENGTH;
-		}
-		last = now;
+uint16_t *IBUS::update() {
+	while ((stream.available() > 0) || (state == WRITE_SENSORVALUES)) {
+		if (state != WRITE_SENSORVALUES) {
+			// only consider a new packet, if no one was received for over 3ms
+			uint32_t now = millis();
+			if (now - last >= PROTOCOL_TIMEGAP){
+				state = GET_LENGTH;
+			}
+			last = now;
 		
-		rc = stream.read();
+			rc = stream.read();
+		}
 		
 		switch (state) {
 			case GET_LENGTH:
 				if ((rc <= PROTOCOL_LENGTH) && (rc > PROTOCOL_OVERHEAD)) {
-					bufferIndex = 0;
-					len = rc - PROTOCOL_OVERHEAD;
-					checksum = 0xFFFF - rc;
+					dataIndex = 0;
+					dataLength = rc - PROTOCOL_OVERHEAD;
+					checksumCalculated = 0xFFFF - rc;
 					state = GET_DATA;
 				}
 				else {
@@ -96,76 +86,80 @@ void IBUS::loop(void) {
 				break;
 				
 			case GET_DATA:
-				buffer[bufferIndex++] = rc;
-				checksum -= rc;
-				if (bufferIndex == len) {
-					state = GET_CHECKSUM_L;
+				data[dataIndex++] = rc;
+				checksumCalculated -= rc;
+				if (dataIndex == dataLength) {
+					state = GET_CHECKSUMLOW;
 				}
 				break;
 			
-			case GET_CHECKSUM_L:
-				chkl = rc;
-				state = GET_CHECKSUM_H;
+			case GET_CHECKSUMLOW:
+				checksumLow = rc;
+				state = GET_CHECKSUMHIGH;
 				break;
 
-			case GET_CHECKSUM_H:
+			case GET_CHECKSUMHIGH:
 				// validate checksum
-				if (checksum == (rc << 8) + chkl) {
+				if (checksumCalculated == (rc << 8) + checksumLow) {
 					// extract sensor address
-					adr = buffer[0] & 0x0F;
-					// validate command
-					if (buffer[0] == PROTOCOL_COMMAND40) {
-						// extract channel data
+					sensorIndex = data[0] & 0x0F;
+					// validate command value
+					if (data[0] == PROTOCOL_COMMAND40) {
+						// extract channel values
 						for (uint8_t i = 1; i < PROTOCOL_CHANNELS * 2 + 1; i += 2) {
-							channel[i / 2] = buffer[i] | (buffer[i + 1] << 8);
+							channelValues[i / 2] = data[i] | (data[i + 1] << 8);
 						}
 						cnt_channelMessage++;
 					} 
-					else if ((adr <= sensorNumber) && (adr > 0) && (len == 1)) {
+					else if ((sensorIndex <= sensorNumber) && (sensorIndex > 0) && (dataLength == 1)) {
 						/*
 						All sensor data commands. 
-						Only process len == 1 (packet length is 4 bytes (with overhead)) to prevent the case the
+						Only process dataLength == 1 (packet length is 4 bytes (with overhead)) to prevent the case the
 						return messages from the UART TX port loop back to the RX port and are processed again. This is extra
 						precaution, since it will also be prevented by PROTOCOL_TIMEGAP
 						*/
-						//delayMicroseconds(100);
-						
-						// TODO: Change to write only one byte per loop / timer interrupt
-						switch (buffer[0] & 0xF0) {
-							case PROTOCOL_COMMAND_DISCOVER: // 0x80, discover sensor
-								// echo discover command: 0x04, 0x81, 0x7A, 0xFF
-								stream.write(0x04);
-								stream.write(PROTOCOL_COMMAND_DISCOVER + adr);
-								checksum = 0xFFFF - (0x04 + PROTOCOL_COMMAND_DISCOVER + adr);
-								cnt_pollMessage++;
-								break;
-							case PROTOCOL_COMMAND_TYPE: // 0x90, send sensor type
-								// echo sensor type command: 0x06 0x91 0x00 0x02 0x66 0xFF
-								stream.write(0x06);
-								stream.write(PROTOCOL_COMMAND_TYPE + adr);
-								stream.write(sensorType[adr]);
-								stream.write(0x02); // always this value - unknown
-								checksum = 0xFFFF - (0x06 + PROTOCOL_COMMAND_TYPE + adr + sensorType[adr] + 2);
-								break;
-							case PROTOCOL_COMMAND_VALUE: // 0xA0, send sensor data
-								// echo sensor value command: 0x06 0x91 0x00 0x02 0x66 0xFF
-								stream.write(0x06);
-								stream.write(PROTOCOL_COMMAND_VALUE + adr);
-								stream.write(sensorValue[adr] & 0xFF);
-								stream.write(sensorValue[adr] >> 8);
-								checksum = 0xFFFF - (0x06 + PROTOCOL_COMMAND_VALUE + adr + (sensorValue[adr] >> 8) + (sensorValue[adr] & 0xFF));
-								cnt_sentMessage++;
-								break;
-							default:
-								adr = 0; // unknown command, prevent sending checksum
-								break;
-						}
-						if (adr > 0) {
-							stream.write(checksum & 0xFF);
-							stream.write(checksum >> 8);
-						}
+						state = WRITE_SENSORVALUES;
+						break;
 					}
 				}
+				state = DISCARD;
+				break;
+			case WRITE_SENSORVALUES:
+				// This case was introduced to replace a delay between reading and writing to the iBus. Now writing happens during next update.
+				switch (data[0] & 0xF0) {
+					case PROTOCOL_COMMAND_DISCOVER: // 0x80, discover sensor
+						// echo discover command: 0x04, 0x81, 0x7A, 0xFF
+						stream.write(0x04);
+						stream.write(PROTOCOL_COMMAND_DISCOVER + sensorIndex);
+						checksumCalculated = 0xFFFF - (0x04 + PROTOCOL_COMMAND_DISCOVER + sensorIndex);
+						cnt_pollMessage++;
+						break;
+					case PROTOCOL_COMMAND_TYPE: // 0x90, send sensor type
+						// echo sensor type command: 0x06 0x91 0x00 0x02 0x66 0xFF
+						stream.write(0x06);
+						stream.write(PROTOCOL_COMMAND_TYPE + sensorIndex);
+						stream.write(sensorType[sensorIndex]);
+						stream.write(0x02); // always this value - unknown
+						checksumCalculated = 0xFFFF - (0x06 + PROTOCOL_COMMAND_TYPE + sensorIndex + sensorType[sensorIndex] + 2);
+						break;
+					case PROTOCOL_COMMAND_VALUE: // 0xA0, send sensor data
+						// echo sensor value command: 0x06 0x91 0x00 0x02 0x66 0xFF
+						stream.write(0x06);
+						stream.write(PROTOCOL_COMMAND_VALUE + sensorIndex);
+						stream.write(sensorValue[sensorIndex] & 0xFF);
+						stream.write(sensorValue[sensorIndex] >> 8);
+						checksumCalculated = 0xFFFF - (0x06 + PROTOCOL_COMMAND_VALUE + sensorIndex + (sensorValue[sensorIndex] >> 8) + (sensorValue[sensorIndex] & 0xFF));
+						cnt_sentMessage++;
+						break;
+					default:
+						sensorIndex = 0; // unknown command, prevent sending checksum
+						break;
+				}
+				if (sensorIndex > 0) {
+					stream.write(checksumCalculated & 0xFF);
+					stream.write(checksumCalculated >> 8);
+				}
+				
 				state = DISCARD;
 				break;
 			case DISCARD:
@@ -174,19 +168,10 @@ void IBUS::loop(void) {
 				break;
 		}
 	}
-}
-
-uint16_t IBUS::readChannel(uint8_t channelNumber) {
-	if (channelNumber < PROTOCOL_CHANNELS) {
-		return channel[channelNumber];
-	}
-	else {
-		return 0;
-	}
+	return channelValues;
 }
 
 uint8_t IBUS::addSensor(uint8_t type) {
-	// add a sensor, return sensor number
 	if (sensorNumber < SENSORMAX) {
 		++sensorNumber;
 		sensorType[sensorNumber] = type;
@@ -194,8 +179,8 @@ uint8_t IBUS::addSensor(uint8_t type) {
 	return sensorNumber;
 }
 
-void IBUS::setSensorMeasurement(uint8_t adr, uint16_t value){
-	if (adr <= sensorNumber) {
-		sensorValue[adr] = value;	
+void IBUS::setSensorMeasurement(uint8_t sensorIndex, uint16_t value){
+	if (sensorIndex <= sensorNumber) {
+		sensorValue[sensorIndex] = value;	
 	}
 }
--- src/iBus.h
+++ src/iBus.h
@@ -13,60 +13,60 @@
 #ifndef IBUS_H
 #define IBUS_H
 
-//#include <inttypes.h>
+#include <Arduino.h>
 
+// TODO: What is this needed for?
 #define IBUSS_INTV 0	// Internal voltage (in 0.01)
 #define IBUSS_TEMP 1	// Temperature (in 0.1 degrees, where 0=-40'C)
 #define IBUSS_RPM  2	// RPM
 #define IBUSS_EXTV 3	// External voltage (in 0.01)
 
-//class HardwareSerial;
-//class Stream;
-
 class IBUS {
 
 	public:
-	void begin(HardwareSerial& serial, IntervalTimer timer, int8_t rxPin, int8_t txPin);
-	uint16_t readChannel(uint8_t channelNumber);	// read channel 0..9
-	uint8_t addSensor(uint8_t type);	// usually 2, returns address
-	void setSensorMeasurement(uint8_t adr, uint16_t value);
-
-	void loop(void);	// used internally for interrupt handling, but needs to be defined as public
+	void begin(HardwareSerial& serial, int8_t rxPin, int8_t txPin);
+	
+	uint16_t *update();	// update and return current channel values
+	
+	uint8_t addSensor(uint8_t type);	// add sensor and return sensor index
+	void setSensorMeasurement(uint8_t sensorIndex, uint16_t value);
 	
 	uint16_t cnt_pollMessage;		// count of sensor poll messages
 	uint16_t cnt_sentMessage;		// count of sent messages
 	uint16_t cnt_channelMessage;	// count of received messages
 	
 	private:
-	enum State {GET_LENGTH, GET_DATA, GET_CHECKSUM_L, GET_CHECKSUM_H, DISCARD};
+	enum State {GET_LENGTH, GET_DATA, GET_CHECKSUMLOW, GET_CHECKSUMHIGH, WRITE_SENSORVALUES, DISCARD};
 	
-	// packet is <length><cmd><data><chkl><chkh>
+	// protocol		<length><commandValue><channelValues><checksumLow><checksumHigh>
+	// data			<commandValue><channelValues>
+	// overhead		<length><checksumLow><checksumHigh>
 	// length: number of bytes inside packet
 	static const uint8_t PROTOCOL_LENGTH = 0x20;
-	static const uint8_t PROTOCOL_OVERHEAD = 3;	// overhead: length, chkl, chkh
+	static const uint8_t PROTOCOL_OVERHEAD = 3;
 	static const uint8_t PROTOCOL_TIMEGAP = 3;	// packet is received every ~7 ms, so using 3 ms for the time gap is feasible
 	static const uint8_t PROTOCOL_CHANNELS = 14;
-	static const uint8_t PROTOCOL_COMMAND40 = 0x40;	// command to send channels (always 0x40)
+	static const uint8_t PROTOCOL_COMMAND40 = 0x40;			// command to send channelValues (always 0x40)
 	static const uint8_t PROTOCOL_COMMAND_DISCOVER = 0x80;	// command to discover sensor (4 highest bits)
-	static const uint8_t PROTOCOL_COMMAND_TYPE = 0x90;	// command for sensor type (4 highest bits)
-	static const uint8_t PROTOCOL_COMMAND_VALUE = 0xA0;	// command for sensor data (4 highest bits)
+	static const uint8_t PROTOCOL_COMMAND_TYPE = 0x90;		// command for sensor type (4 highest bits)
+	static const uint8_t PROTOCOL_COMMAND_VALUE = 0xA0;		// command for sensor data (4 highest bits)
 	static const uint8_t SENSORMAX = 10;	// maximum number of sensors
-	static const uint16_t TIMER_INTERVAL = 100;	// timer interval in microseconds - There is (7 - PROTOCOL_TIMEGAP) ms for PROTOCOL_LENGTH bytes (example:  4 ms for 32 byte = 125 us for 1 byte)
 	
-	uint8_t rc;	// received byte from remote control
-	uint8_t state;	// state machine state for iBus protocol
-	uint8_t adr;	// sensor address
 	HardwareSerial stream;	// serial port
+	uint8_t rc;	// received byte
+	uint8_t state;	// iBus protocol state
 	uint32_t last;	// ms since prior message
-	uint8_t buffer[PROTOCOL_LENGTH];	// message buffer
-	uint8_t bufferIndex;	// buffer index
-	uint8_t len;	// number of bytes for cmd, data
-	uint16_t channel[PROTOCOL_CHANNELS];	// servo data received
-	uint16_t checksum;	// calculated checksum
-	uint8_t chkl;	// lower byte of received checksum
-	uint8_t sensorType[SENSORMAX];	// sensor types for defined sensors
-	uint16_t sensorValue[SENSORMAX];	// sensor data for defined sensors
+	uint8_t data[PROTOCOL_LENGTH - PROTOCOL_OVERHEAD];	// data
+	uint8_t dataIndex;	// data index
+	uint8_t dataLength;	// data length
+	uint16_t channelValues[PROTOCOL_CHANNELS];	// received channel values
+	uint16_t checksumCalculated;	// calculated checksum
+	uint8_t checksumLow;			// lower byte of received checksum
+	
+	uint8_t sensorIndex;	// sensor index
 	uint8_t sensorNumber;	// number of sensors
+	uint8_t sensorType[SENSORMAX];		// sensor types for defined sensors
+	uint16_t sensorValue[SENSORMAX];	// sensor data for defined sensors
 };
 
 #endif
